8f196f31b9b6059aa2041bab205453ef11c4d019
locwa <94048557+locwa@users.noreply.github.com>
2025-02-13 20:04:17 +0800

    added full logo component

---
+++ resources/views/components/logos/logo-full.blade.php
@@ -0,0 +1,4 @@
+<div {{ $attributes->merge(['class' => 'flex items-center py-2']) }}>
+    <x-logos.crest class="h-14 w-14 mr-3"></x-logos.crest>
+    <x-logos.logo class="h-28 w-28"></x-logos.logo>
+</div>
