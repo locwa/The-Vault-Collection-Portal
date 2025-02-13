bc6ceef9be08917d1bec565681a5fbec4b1522cb
locwa <94048557+locwa@users.noreply.github.com>
2025-02-13 17:31:14 +0800

    added optional title element on the guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,10 +19,13 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
+            <div class="text-center">
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-logos.crest class="w-32 h-32"></x-logos.crest>
                 </a>
+                @isset($title)
+                    <h4 class="text-xl font-semibold pt-6 dark:text-gray-100">{{ $title }}</h4>
+                @endisset
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
