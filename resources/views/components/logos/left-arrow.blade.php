555f98a8421335e8e261b94ed79027219d136d55
locwa <94048557+locwa@users.noreply.github.com>
2025-02-13 17:26:11 +0800

    added logo components

---
+++ resources/views/components/logos/left-arrow.blade.php
@@ -0,0 +1,3 @@
+<svg width=" 100%" height=" 100%" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6.34961 1.175L2.53294 5L6.34961 8.825L5.17461 10L0.17461 5L5.17461 1.35697e-07L6.34961 1.175Z" fill="currentColor"></path>
+</svg>
---
+++ resources/views/components/logos/right-arrow.blade.php
@@ -0,0 +1,3 @@
+<svg width=" 100%" height=" 100%" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 8.825L3.81667 5L0 1.175L1.175 0L6.175 5L1.175 10L0 8.825Z" fill="currentColor"></path>
+</svg>
