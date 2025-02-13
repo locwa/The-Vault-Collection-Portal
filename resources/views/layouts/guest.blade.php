0f4e39234e736540da42facf904ede94f77845c2
locwa <94048557+locwa@users.noreply.github.com>
2025-02-13 20:14:14 +0800

    changed color of login and register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-[#0f0f0f]">
             <div class="text-center">
                 <x-logos.crest class="w-32 h-32"></x-logos.crest>
                 @isset($title)
@@ -26,7 +26,7 @@
                 @endisset
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-[#1f1f1f] shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
         </div>
