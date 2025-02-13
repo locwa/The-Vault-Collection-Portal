35472862833c5bcdfb1aeb17b9d49a28a6707d67
locwa <94048557+locwa@users.noreply.github.com>
2025-02-13 21:30:42 +0800

    added blocking on inventory page

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -5,4 +5,26 @@
         </h2>
     </x-slot>
 
+    <div class="px-12 flex flex-col items-center mx-14">
+        <div class="w-full pt-12">
+            <div class="bg-white dark:bg-[#1f1f1f] overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 w-full">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full h-[100vh] mt-12 flex justify-between gap-10">
+            <div class="h-[90vh] w-96 bg-gray-300 dark:bg-[#1f1f1f] rounded"></div>
+            <div class="flex flex-wrap gap-5">
+                <div class="h-80 w-72 dark:bg-[#1f1f1f] rounded"></div>
+                <div class="h-80 w-72 dark:bg-[#1f1f1f] rounded"></div>
+                <div class="h-80 w-72 dark:bg-[#1f1f1f] rounded"></div>
+                <div class="h-80 w-72 dark:bg-[#1f1f1f] rounded"></div>
+                <div class="h-80 w-72 dark:bg-[#1f1f1f] rounded"></div>
+            </div>
+        </div>
+
+    </div>
+
 </x-app-layout>
