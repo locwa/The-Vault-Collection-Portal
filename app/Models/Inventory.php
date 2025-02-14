aceedcc545530d419c34d9b5dc6bc7f26652fec6
locwa <94048557+locwa@users.noreply.github.com>
2025-02-14 16:40:14 +0800

    added migration and model for inventory

---
+++ app/Models/Inventory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $table = 'inventory';
+    protected $fillable = [
+        'make',
+        'model',
+        'year',
+        'price',
+        'mileage',
+        'description',
+        'status',
+        'sold_at',
+        'photo_header'];
+}
---
+++ database/migrations/2025_02_14_083153_inventory.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->id();
+            $table->string('make');
+            $table->string('model');
+            $table->string('year');
+            $table->string('photo_header');
+            $table->integer('price');
+            $table->integer('mileage');
+            $table->integer('description');
+            // 0 for available, 1 for sold
+            $table->boolean('status');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('sold_at');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('inventory');
+    }
+};
