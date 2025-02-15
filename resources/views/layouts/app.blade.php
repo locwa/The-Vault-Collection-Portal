33a52cffb9d4c4cd356f345e4cb80c38193ea35c
locwa <94048557+locwa@users.noreply.github.com>
2025-02-15 19:17:56 +0800

    changed title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=1920, height=1080, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Portal | The Vault</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
