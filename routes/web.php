53f4aeff9ababf169cdf4473bd7cfed17c3c019c
locwa <94048557+locwa@users.noreply.github.com>
2025-02-26 20:40:13 +0800

    removed unnecessary class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\InventoryController;
