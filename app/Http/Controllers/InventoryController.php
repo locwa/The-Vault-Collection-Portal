bae01c95603d51cb35ad25c4ea6df767e6e4c03f
locwa <94048557+locwa@users.noreply.github.com>
2025-02-26 11:38:40 +0800

    removed unused model

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use App\Models\Inventory;
-use App\Models\Sales;
 use Illuminate\Support\Facades\Storage;
 
 class InventoryController extends Controller
